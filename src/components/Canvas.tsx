a70bafb9e6134d7066bf244be5f543f0a2873310
MichaelShawUK <MichaelShawUK@gmail.com>
2023-07-16 15:50:07 +0100

    Animate tunnel on canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,31 +1,93 @@
 import { useRef, useEffect } from "react";
 
+interface RingType {
+  x: number;
+  y: number;
+  radius: number;
+  growthFactor: number;
+  draw(): void;
+  update(): void;
+}
+
+function ring(
+  x: number,
+  y: number,
+  radius: number,
+  growthFactor: number,
+  ctx: CanvasRenderingContext2D
+) {
+  return {
+    x,
+    y,
+    radius,
+    growthFactor,
+
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.strokeStyle = "rgba(255, 0, 255, 0.1)";
+      ctx.stroke();
+    },
+
+    update() {
+      this.draw();
+      this.radius *= this.growthFactor;
+    },
+  };
+}
+
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  function init() {
-    const canvas = canvasRef.current;
-    if (!canvas) throw new Error("No canvas element");
-    const ctx = canvas.getContext("2d");
-    if (!ctx) throw new Error("Could not get context");
-    return { canvas, ctx };
-  }
-
-  function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = "#222";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white";
-  }
-
   useEffect(() => {
-    const { canvas, ctx } = init();
-    draw(canvas, ctx);
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const mouse = {
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+    };
+
+    canvas.addEventListener("mousemove", (e) => {
+      mouse.x = e.offsetX;
+      mouse.y = e.offsetY;
+    });
 
     window.addEventListener("resize", () => {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
-      draw(canvas, ctx);
     });
+
+    const rings: RingType[] = [];
+
+    function generateRing() {
+      if (!canvas || !ctx) return;
+      setTimeout(() => {
+        rings.push(ring(mouse.x, mouse.y, 5, 1.03, ctx));
+        generateRing();
+      }, 50);
+    }
+
+    function animate() {
+      window.requestAnimationFrame(animate);
+      if (!canvas || !ctx) return;
+      ctx.fillStyle = "rgba(0, 0, 50, 0.5)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      rings.forEach((ring, index) => {
+        const maxRadius = Math.max(canvas.width, canvas.height);
+        if (ring.radius > maxRadius) {
+          rings.splice(index, 1);
+        } else {
+          ring.update();
+        }
+      });
+    }
+
+    animate();
+    generateRing();
   }, []);
 
   return (
